1ffe6bc7a0c0b6565d7c47eed2fe96166e15db6b
Kushal-Marthak <kushalmarthak42@gmail.com>
2025-12-29 11:24:26 +0530

    Add files via upload

---
+++ Data type.py
@@ -0,0 +1,50 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+x="Hello World"
+print (x)
+Hello World
+x=20
+print (x)
+20
+x=20.5
+print (x)
+20.5
+x=1 J
+SyntaxError: invalid syntax
+x=1J
+print (x)
+1j
+x=["apple","banana","cherry"]
+print (x)
+['apple', 'banana', 'cherry']
+x=("apple","banana","cherry")
+\
+print (x)
+('apple', 'banana', 'cherry')
+x="True"
+print (x)
+True
+
+
+
+
+a = 10
+b = 3
+print ("Arithmetic Operations")
+Arithmetic Operations
+print (a + b)
+13
+print (a - b)
+7
+print (a * b)
+30
+print (a / b)
+3.3333333333333335
+print (a // b)
+3
+print (a % b)
+1
+print (a ** b)
+1000
+
+
