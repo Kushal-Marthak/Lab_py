801a6307370a3bfc394bce19bc14e62e2b320e74
Kushal-Marthak <kushalmarthak42@gmail.com>
2026-01-07 08:12:45 +0530

    Add files via upload

---
+++ datatypeexe.py
@@ -0,0 +1,28 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+x="Hello World"
+print (x)
+Hello World
+x="True"
+print (x)
+True
+x=("apple","banana","cherry")
+print (x)
+('apple', 'banana', 'cherry')
+x=["apple","banana","cherry"]
+print (x)
+['apple', 'banana', 'cherry']
+x={"apple","banana","cherry"}
+print (x)
+{'cherry', 'banana', 'apple'}
+x=1 J
+SyntaxError: invalid syntax
+x=1j;
+print (x)
+1j
+x=20.5
+print (x)
+20.5
+x=""
+print (x)
+
