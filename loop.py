c3ba6b4fe9acbb52dda293acc55161e8fcdfc5c2
Kushal-Marthak <kushalmarthak42@gmail.com>
2026-02-16 11:23:48 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,33 @@
+#1
+for i in range(1, 11):
+    print(i)
+#2
+for i in range(1, 21):
+    if i % 2 == 0:
+        print(i)
+#3
+for i in range(1, 16):
+    if i % 2 != 0:
+        print(i)
+#4
+for i in range(1, 11):
+    print("5 x", i, "=", 5 * i)
+#5
+name = "Atmiya"
+for letter in name:
+    print(letter)
+#6
+total = 0
+for i in range(1, 6):
+    total = total + i
+print("Sum is:", total)
+#7
+numbers = [10, 20, 30, 40]
+for n in numbers:
+    print(n)
+#8
+for i in range(1,6):
+    print(i)
+#9
+for i in range (3):
+    print("hello")
